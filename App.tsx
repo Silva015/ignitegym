1be5af5849458551049e9fdb7194379962ea48a0
Silva015 <tutuscarneiro@gmail.com>
2024-09-24 14:01:31 -0300

    Assets da Aplicação

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { StatusBar, Text, View } from "react-native";
 import {
   useFonts,
   Roboto_400Regular,
@@ -16,8 +16,14 @@ export default function App() {
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
+        backgroundColor: "#202024",
       }}
     >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Text>Home</Text> : <Text>Loading...</Text>}
     </View>
   );

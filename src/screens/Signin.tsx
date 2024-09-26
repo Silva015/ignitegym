a045b8444ab27e14034ed832c200bea4fa1d7a0a
Silva015 <tutuscarneiro@gmail.com>
2024-09-26 14:29:48 -0300

    Utilizando SVG

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,6 +1,7 @@
-import { Image, VStack } from "@gluestack-ui/themed";
+import { Center, Image, VStack, Text } from "@gluestack-ui/themed";
 
 import BackgroundImg from "@assets/background.png";
+import Logo from "@assets/logo.svg";
 
 export function SignIn() {
   return (
@@ -13,6 +14,14 @@ export function SignIn() {
         alt="Pessoas treinando"
         position="absolute"
       />
+
+      <Center my="$24">
+        <Logo />
+
+        <Text color="$gray100" fontSize="$sm">
+          Treine sua mente e o seu corpo
+        </Text>
+      </Center>
     </VStack>
   );
 }

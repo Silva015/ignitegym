d9d850719d994ec8e94b08902ef6fda4fa2fd519
Silva015 <tutuscarneiro@gmail.com>
2024-09-30 12:57:38 -0300

    Finalizando os Inputs

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -28,8 +28,12 @@ export function SignIn() {
         <Center gap="$2">
           <Heading color="$gray100"> Acesse a conta </Heading>
 
-          <Input placeholder="E-mail" />
-          <Input placeholder="Senha" />
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Input placeholder="Senha" secureTextEntry />
         </Center>
       </VStack>
     </VStack>

68ddeef97ac56166a421efb919ef35669fcc3202
Silva015 <tutuscarneiro@gmail.com>
2024-10-03 11:12:44 -0300

    Alterando os ícones da Bottom Navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,6 +2,12 @@ import {
   createBottomTabNavigator,
   BottomTabNavigationProp,
 } from "@react-navigation/bottom-tabs";
+import { gluestackUIConfig } from "../../config/gluestack-ui.config";
+
+import HomeSvg from "@assets/home.svg";
+import HistorySvg from "@assets/history.svg";
+import ProfileSvg from "@assets/profile.svg";
+
 import { Home } from "@screens/Home";
 import { Exercise } from "@screens/Exercise";
 import { Profile } from "@screens/Profile";
@@ -19,6 +25,9 @@ export type AppNavigatorRotesProps = BottomTabNavigationProp<AppRoutes>;
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
+  const { tokens } = gluestackUIConfig;
+  const iconSize = tokens.space[6];
+
   return (
     <Navigator
       screenOptions={{
@@ -26,9 +35,35 @@ export function AppRoutes() {
         tabBarShowLabel: false,
       }}
     >
-      <Screen name="home" component={Home} />
-      <Screen name="history" component={History} />
-      <Screen name="profile" component={Profile} />
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+
+      <Screen
+        name="history"
+        component={History}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HistorySvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+
+      <Screen
+        name="profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <ProfileSvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
       <Screen name="exercise" component={Exercise} />
     </Navigator>
   );

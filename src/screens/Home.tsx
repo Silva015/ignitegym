2e61942e1fd9bb6810fb6fe7e84fb085a01ebcab
Silva015 <tutuscarneiro@gmail.com>
2024-10-18 15:35:57 -0300

    Header da Lista de Exercícios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,27 +1,51 @@
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { HStack, VStack } from "@gluestack-ui/themed";
+import { FlatList, Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
 import { useState } from "react";
 
 export function Home() {
-  const [groupSelected, setGroupSelected] = useState("costa");
+  const [groups, setGroups] = useState([
+    "Costas",
+    "Ombro",
+    "Tríceps",
+    "Bíceps",
+    "Peito",
+    "Perna",
+    "Abdômen",
+  ]);
+  const [groupSelected, setGroupSelected] = useState("Costas");
 
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <HStack>
-        <Group
-          name="Costas"
-          isActive={groupSelected === "costa"}
-          onPress={() => setGroupSelected("costa")}
-        />
 
-        <Group
-          name="Ombro"
-          isActive={groupSelected === "ombro"}
-          onPress={() => setGroupSelected("ombro")}
-        />
-      </HStack>
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item as string}
+        renderItem={({ item }) => (
+          <Group
+            name={item as string}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item as string)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 32 }}
+        style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+      />
+
+      <VStack px="$8">
+        <HStack justifyContent="space-between" mb="$5">
+          <Heading color="$gray200" fontSize="$md">
+            Exercícios
+          </Heading>
+
+          <Text color="$gray200" fontSize="$sm" fontFamily="$body">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
